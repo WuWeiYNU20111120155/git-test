c237ec762c066169d8927e438cc191789a8ca8e5
wuwei <1099284874@qq.com>
2018-08-13 21:05:24 +0800

    add 1.java

---
+++ 1.java
@@ -0,0 +1,15 @@
+
+public class JavaDay001_1 {
+ 
+    public static void main(String[] args) {
+ 
+        System.out.println(args);
+    }
+}
+ 
+    public static void main(String[] args) {
+ 
+        System.out.println(args);
+    }
+}
+
